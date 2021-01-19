988608d189a3236a6dac21f0c070c528d38f0152
pierrecariou <pierrecariou@outlook.fr>
2021-01-19 15:59:26 +0100

    added users to ep

--- srcs/mysql/srcs/script.sh
+++ srcs/mysql/srcs/script.sh
@@ -4,6 +4,7 @@ rc-service mariadb start
 
 #create wp db
 mysql -u root -e 'CREATE DATABASE wordpress;'
+mysql -u root wordpress < /wordpress.sql
 
 #create db admin
 mysql -u root -e "CREATE USER 'pcariou'@'%' IDENTIFIED BY 'user42';GRANT ALL PRIVILEGES ON *.* TO 'pcariou'@'%' WITH GRANT OPTION;USE wordpress;FLUSH PRIVILEGES;"
