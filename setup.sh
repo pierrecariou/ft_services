a020269c979c927911d891d8c376c1c6dddf7260
pierrecariou <pierrecariou@outlook.fr>
2021-01-31 15:56:58 +0100

    script improvment

--- setup.sh
+++ setup.sh
@@ -2,18 +2,21 @@
 
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0;37m'
 
 echo -e "$BOLD 
  _ _   _   _  _         _  _  _
 |_ |  |_  |_ |_|\  / | |  |_ |_  
 .  .    . .  .   ..  . .  .    .
-|  | _ _| |_ |\  \/  | |_ |_  _| $NORMAL\n\n\n"
+|  | _ _| |_ |\  \/  | |_ |_  _| $NORMAL\n\n"
 
 SERVICES=(influxdb grafana nginx mysql wordpress phpmyadmin ftps)
 
 #Downolading kubernetes's setup
 
-sudo apt-get update
+sudo apt-get update > /dev/null
 
 if ! command -v curl > /dev/null
 then
@@ -29,9 +32,9 @@ fi
 
 if ! grep -E --color 'vmx|svm' /proc/cpuinfo > /dev/null
 then
-	echo "You need to authorize virtualization in your bios"
-	echo "Virtualbox will not be used..."
-	echo "you may need to execute this command before a reboot of your system (to be able to use docker driver) : 'sudo usermod -aG docker $(whoami)'"
+	echo "Virtualization in your bios is not activated"
+	echo "Docker driver will be used"
+	echo "You may need to execute this command before a reboot of your system: sudo usermod -aG docker $(whoami)"
 elif ! command -v virtualbox > /dev/null
 then
 	echo "Downloading virtualbox..."
@@ -74,10 +77,15 @@ running_minikube()	{
 running_minikube
 
 metallb_setup()	{
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-	cat srcs/metallab.yaml | kubectl create -f -
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml > output.log
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml >> output.log
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" >> output.log
+	cat srcs/metallab.yaml | kubectl create -f - >> output.log
+	lines=$(cat output.log | grep created | wc -l)
+	if [ $lines -eq 17 ]
+	then
+		echo "Metallab is working"
+	fi
 }
 
 metallb_setup
@@ -86,14 +94,36 @@ metallb_setup
 eval $(minikube docker-env)
 
 build_and_deploy()	{
-	docker build -t $1 srcs/$1/.
-	kubectl apply -f srcs/$1/$1.yaml
+	length=$(echo -n $1 | wc -m)
+	echo -n "Building $1 image..."
+	docker build -t $1 srcs/$1/. > output.log
+	if ! grep Successfully output.log > /dev/null
+	then
+		echo "Failed"
+	else
+		for ((i=0; i<11-$length; i++)){
+			echo -n ' '
+		}
+		echo -n "Success!"
+		echo -e '\U1F917'
+	fi
+	kubectl apply -f srcs/$1/$1.yaml > output.log
+	if ! grep created output.log > /dev/null
+	then
+		echo -ne "$RED"
+		echo -e "Service not deployed $WHITE"
+	else
+		echo -ne "$GREEN"
+		echo -e "Service $1 deployed $WHITE"
+	fi
 }
 
 for i in "${SERVICES[@]}"
 do
 	build_and_deploy $i
 done
+
+rm output.log
 
 minikube dashboard
 
