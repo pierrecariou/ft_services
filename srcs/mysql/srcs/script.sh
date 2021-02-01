102678429d5adcac756a6049e87097c2714cdf17
pierrecariou <pierrecariou@outlook.fr>
2021-02-01 12:36:25 +0100

    mysql pvc

--- srcs/mysql/srcs/script.sh
+++ srcs/mysql/srcs/script.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
+#starting mysql
+mysql_install_db -u root
 rc-service mariadb start
 
 #create wp db
-mysql -u root -e 'CREATE DATABASE wordpress;'
-mysql -u root wordpress < /wordpress.sql
+if ! mysql -u root -e 'USE wordpress'
+then
+	mysql -u root -e 'CREATE DATABASE wordpress;'
+	mysql -u root wordpress < /wordpress.sql
+fi
 
 #create db admin
 mysql -u root -e "CREATE USER 'pcariou'@'%' IDENTIFIED BY 'user42';GRANT ALL PRIVILEGES ON *.* TO 'pcariou'@'%' WITH GRANT OPTION;USE wordpress;FLUSH PRIVILEGES;"
 
 telegraf
-#/bin/ash
-#while true
-#do
-#	sleep 1
-#done
